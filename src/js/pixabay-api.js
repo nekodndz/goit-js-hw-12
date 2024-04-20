9ee7732b160a7a6f05b718446e9f9eeef48d8b7f
nekodndz <138133074+nekodndz@users.noreply.github.com>
2024-04-20 20:21:09 +0300

    corrected

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -9,28 +9,16 @@ const perPage = 15;
 async function getPictures(name, page) {
   const KEY = '43226276-a07a0c17e428cfffb021b9b05';
 
-  try {
-    if (name.includes(' ')) {
-      name = name.replace(/\s+/g, '+');
-    }
-
-    const response = await axios.get(`https://pixabay.com/api/`, {
-      params: {
-        key: KEY,
-        q: name,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        page: page,
-        per_page: perPage,
-      },
-    });
-    return response;
-  } catch (error) {
-    iziToast.error({
-      title: 'Error',
-      message: 'Sorry! The site is currently unavailable. Please try later!',
-    });
-    console.error(error.message);
-  }
+  const response = await axios.get(`https://pixabay.com/api/`, {
+    params: {
+      key: KEY,
+      q: name,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: page,
+      per_page: perPage,
+    },
+  });
+  return response;
 }
